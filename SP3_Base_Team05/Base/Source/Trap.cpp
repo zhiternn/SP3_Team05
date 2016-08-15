5e4f08baaf53d027aaafcd185361aa82b1368a10
Wayne Lee <yuckar97@gmail.com>
2016-08-15 17:56:25 +0800

    Added simple stufferinos pepperinos dankerinos

--- SP3_Base_Team05/Base/Source/Trap.cpp
+++ SP3_Base_Team05/Base/Source/Trap.cpp
@@ -18,6 +18,7 @@ Vector3 Trap::getPosition()
 void Trap::SpawnTrap(Vector3 pos)
 {
 	w_active = true;
-
+	GameObject *m_trap = new GameObject(GameObject::GO_CUBE);
+	m_trap->SetScale(0.5, 0.5, 0.5);
 	//Spawn the GO at [pos] coordinates
 }
