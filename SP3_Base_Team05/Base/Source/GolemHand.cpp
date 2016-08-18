6015cb9891158e10ca6f2d6a57756a9e272c6a24
Reuben Quek Tse Hiang <509n1@CL39G308.sit.nyp.edu.sg>
2016-08-18 17:32:20 +0800

    Added new scene to test for Golem Boss. Modified Behavior of Golem Right Hand

--- SP3_Base_Team05/Base/Source/GolemHand.cpp
+++ SP3_Base_Team05/Base/Source/GolemHand.cpp
@@ -18,7 +18,7 @@ void GolemRightHand::Init(Vector3 pos)
     team = TEAM_ENEMY;
     collider.type = Collider::COLLIDER_BALL;
     mass = 1;
-    destinationCountdown = REACH_CHECKER;
+    destinationCountdown = 5.f;
     speedLimit = 50.f;
     movementSpeed = 10.f;
 
@@ -31,31 +31,72 @@ void GolemRightHand::Update(double dt)
 
     if (!Enemy::UpdateMovement(dt))
     {
-        if (link)
-        {
-            if (pos != link->pos)
-            {
-                Vector3 toLink = (link->pos - pos).Normalized();
-            }
-        }
-    }
-
-    if (vel.IsZero() == false)
-        front = vel.Normalized();
-
-    if (!Enemy::UpdateMovement(dt))
-    {
         if (target)
         {
-            float combinedRadius = scale.x + target->GetScale().x;
-            float offset = Math::RandFloatMinMax(combinedRadius * 1.5f, combinedRadius * 2.0f);
-            Vector3 offsetDir = target->pos - pos;
-            offsetDir.Set(-offsetDir.y, offsetDir.x, offsetDir.z);
+            //float combinedRadius = scale.x + target->GetScale().x;
+            //float offset = Math::RandFloatMinMax(combinedRadius * 2, combinedRadius * 3);
+            //Vector3 offsetDir = target->pos - pos;
+            //offsetDir.Set(-offsetDir.y, offsetDir.x, offsetDir.z);
 
-            Vector3 destination = target->pos + offsetDir.Normalized() * offset;
+            //Vector3 destination = target->pos + offsetDir.Normalized() * offset;
+            //ChangeDestination(MOVETO_TARGET, destination);
+
+            float combinedRadius = scale.x + target->GetScale().x;
+            Vector3 offsetDir = (target->pos - pos);
+            offsetDir.Set(-offsetDir.y, offsetDir.x, offsetDir.z);
+            float offset = combinedRadius * 2;
+
+            Vector3 destination = (target->pos * 0.5) + offsetDir.Normalized();
             ChangeDestination(MOVETO_TARGET, destination);
+            if (Reached(destination))
+            {
+                destination = (target->pos * 2) + offsetDir.Normalized();
+                ChangeDestination(MOVETO_TARGET, destination);
+            }
+
+
+            {//testy stuff
+                static GameObject* hehe = NULL;
+                GameObject* go = FetchGO();
+                go->SetActive(true);
+
+                go->pos = destination;
+                if (hehe)
+                    hehe->SetActive(false);
+                hehe = go;
+            }
         }
+
     }
+
+    //if (!Enemy::UpdateMovement(dt))
+    //{
+    //    if (link)
+    //    {
+    //        if (pos != link->pos)
+    //        {
+    //            Vector3 toLink = (link->pos - pos).Normalized();
+    //        }
+    //    }
+    //}
+
+    //if (vel.IsZero() == false)
+    //    front = vel.Normalized();
+
+    //if (!Enemy::UpdateMovement(dt))
+    //{
+    //    if (target)
+    //    {
+    //        float combinedRadius = scale.x + target->GetScale().x;
+    //        float offset = Math::RandFloatMinMax(combinedRadius * 1.5f, combinedRadius * 2.0f);
+    //        Vector3 offsetDir = target->pos - pos;
+    //        offsetDir.Set(-offsetDir.y, offsetDir.x, offsetDir.z);
+
+    //        Vector3 destination = target->pos + offsetDir.Normalized() * offset;
+    //        ChangeDestination(MOVETO_TARGET, destination);
+    //    }
+    //}
+
 }
 
 void GolemRightHand::LinkTo(Entity* entity)
@@ -83,7 +124,7 @@ void GolemLeftHand::Init(Vector3 pos)
     mass = 1;
     destinationCountdown = REACH_CHECKER;
     speedLimit = 50.f;
-    movementSpeed = 10.0f;
+    movementSpeed = .0f;
 
     captureRatio = 1.f;
 }
