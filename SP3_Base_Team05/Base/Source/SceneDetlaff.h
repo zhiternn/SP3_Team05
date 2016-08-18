e24d2bda42514147a21793704802317027c7789f
Wayne <yuckar97@gmail.com>
2016-08-18 17:53:44 +0800

    Setup Boss Scene for work tomorrow

---
+++ SP3_Base_Team05/Base/Source/SceneDetlaff.h
@@ -0,0 +1,61 @@
+#ifndef SCENE_DETLAFF_H
+#define SCENE_DETLAFF_H
+
+#include "SceneBase.h"
+#include "Camera.h"
+#include "GameObject.h"
+#include "Player.h"
+#include "WeaponList.h"
+#include "ProjectileList.h"
+#include "Summoner.h"
+
+#include <vector>
+
+using std::vector;
+
+class SceneDetlaff : public SceneBase
+{
+	enum GEOMETRY_TYPE
+	{
+		NUM_GEOMETRY = GEOMETRY_TYPE_DEFAULT::GEO_DEFAULT_END
+	};
+public:
+	SceneDetlaff();
+	~SceneDetlaff();
+
+	virtual void Init();
+	virtual void Update(double dt);
+	virtual void Render();
+	virtual void Exit();
+
+	void RenderGPass();
+	void RenderMain();
+	void RenderWorld();
+	void RenderHUD();
+
+	void RenderSkybox();
+	void RenderSkyPlane();
+
+private:
+
+	void PlayerController(double dt);
+	void UpdateGameObjects(double dt);
+	void RenderGameObjects();
+	void RenderGO(GameObject* go);
+
+	float m_worldWidth, m_worldHeight;
+	float m_orthoWidth, m_orthoHeight;
+
+	Mesh* meshList[GEO_DEFAULT_END];
+
+	Camera* mainCamera;
+
+	Player* player;
+
+	Vector3 mousePos_screenBased;
+	Vector3 mousePos_worldBased;
+
+	Summoner* summoner;
+};
+
+#endif
